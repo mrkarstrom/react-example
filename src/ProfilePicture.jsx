b8a2bfdf677ccac49428f28e7be7171ff5fbfc82
Magnus Karström <mrkarstrom81@gmail.com>
2024-01-29 01:56:53 +0100

    feat (ProfilePicture.jsx): Making the image toggle visibility.

--- src/ProfilePicture.jsx
+++ src/ProfilePicture.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-//import PropTypes from 'prop-types';
+ import React, { useState } from 'react';
 
-function ProfilePicture(props) {
-  const imageUrl = '../src/assets/profiole.jpg';
+ function ProfilePicture() {
+   const imageUrl = '../src/assets/profiole.jpg';
+   const [isVisible, setIsVisible] = useState(true);
 
-  const handleClick = () => console.log('Test');
-  return (
-    <div>
-      <img onClick={handleClick} src={imageUrl} />
-    </div>
-  );
-}
+   const handleClick = (e) => {
+     e.stopPropagation(); // Stop the event from propagating to the image
+     setIsVisible((prevVisibility) => !prevVisibility);
+   };
 
-//ProfilePicture.propTypes = {};
+   return (
+     <div className="profile-picture-container" onClick={handleClick}>
+       {isVisible && <img src={imageUrl} alt="Profile" />}
+     </div>
+   );
+ }
 
-export default ProfilePicture;
+ export default ProfilePicture;
