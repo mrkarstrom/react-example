fefd11833dcf23ec29248f1034772fa8ec6e8c0d
Magnus Karström <mrkarstrom81@gmail.com>
2024-01-31 06:20:00 +0100

    add (ProfilePicture.jsx): Added Hide/Show-toggle

--- src/ProfilePicture.jsx
+++ src/ProfilePicture.jsx
@@ -1,19 +1,20 @@
- import React, { useState } from 'react';
+import React, { useState } from 'react';
 
- function ProfilePicture() {
-   const imageUrl = '../src/assets/profiole.jpg';
-   const [isVisible, setIsVisible] = useState(true);
+function ProfilePicture() {
+  const imageUrl = '../src/assets/profiole.jpg';
+  const [isVisible, setIsVisible] = useState(true);
 
-   const handleClick = (e) => {
-     e.stopPropagation(); // Stop the event from propagating to the image
-     setIsVisible((prevVisibility) => !prevVisibility);
-   };
+  const handleClick = (e) => {
+    e.stopPropagation(); // Stop the event from propagating to the image
+    setIsVisible((prevVisibility) => !prevVisibility);
+  };
 
-   return (
-     <div className="profile-picture-container" onClick={handleClick}>
-       {isVisible && <img src={imageUrl} alt="Profile" />}
-     </div>
-   );
- }
+  return (
+    <div className="profile-picture-container" onClick={handleClick}>
+      Click Image to {isVisible ? <span>hide</span> : <span>show</span>}
+      {isVisible && <img src={imageUrl} alt="Profile" />}
+    </div>
+  );
+}
 
- export default ProfilePicture;
+export default ProfilePicture;
